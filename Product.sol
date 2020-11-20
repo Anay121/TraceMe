9f1ee42741aa5ddb9f090dc6430e4aef06684489
anay121 <anay.kulkarni10@gmail.com>
2020-11-20 14:31:55 +0530

    Edits

--- Product.sol
+++ Product.sol
@@ -13,20 +13,6 @@ contract SupplyChain {
         string role;
     }
     
-    mapping(uint => Participant) participant;   // userId -> User Object
-    uint uid = 0;
-    
-    function addParticipant (string memory _username, string memory _password, string memory _fullname, string memory _role) public returns(uint) {
-        Participant memory p = Participant(_username, _password, _fullname, _role);
-        participant[++uid] = p;
-        return(uid);
-    }
-    // TODO:login?
-    
-    function getParticipant (uint _id) public view returns(Participant memory) {
-        return( participant[_id]);
-    }
-
     // Trace
     struct Trace {
         string time;
@@ -36,7 +22,6 @@ contract SupplyChain {
     }
     
     // Product
-    
     struct Product {
         string productName;
         // mapping(string => string) productProperties;
@@ -47,10 +32,31 @@ contract SupplyChain {
         uint currentOwnerId;
     }
     
+    mapping(uint => Participant) participant;   // userId -> User Object
+    uint[] participantIds;
+    uint uid = 0;
+    
     mapping (uint => uint[]) productsOwned; // ParticipantId -> all products 
-    mapping (uint => Trace[]) productTrace; // productId -> Trace array
+    mapping (uint => Trace[]) public productTrace; // productId -> Trace array
     mapping (uint => Product) products;      // productId -> Product Object
     uint pid = 0;
+    
+    modifier onlyOwner(uint _senderId, uint _productId){
+        require(products[_productId].currentOwnerId == _senderId, "Its not yours");
+        _;
+    }
+    
+    function addParticipant (string memory _username, string memory _password, string memory _fullname, string memory _role) public returns(uint) {
+        Participant memory p = Participant(_username, _password, _fullname, _role);
+        participant[++uid] = p;
+        participantIds.push(uid);
+        return(uid);
+    }
+    // TODO:login?
+    
+    function getParticipant (uint _id) public view returns(Participant memory) {
+        return( participant[_id]);
+    }
     
     function addProduct( 
         string memory _productName, int[] memory _parentId, uint[] memory _childrenId, uint _currentOwnerId, 
@@ -82,7 +88,7 @@ contract SupplyChain {
         return(products[_pid]);
     }
     
-    function removeFromOwner(uint _ownerId, uint _productId) private{
+    function removeFromOwner(uint _ownerId, uint _productId) public onlyOwner(_ownerId, _productId) {
         bool flag = false;
         for(uint i = 0; i < productsOwned[_ownerId].length - 1; i++){
             
@@ -99,61 +105,39 @@ contract SupplyChain {
 
     // transfer ownership
     
-    modifier onlyOwner(uint _senderId, uint _productId){
-        require(products[_productId].currentOwnerId == _senderId, "Its not yours");
-        _;
-    }
-    
     function TransferOwnership(
         uint _senderId, uint _receiverId, uint _productId, string memory _location, string memory _time
-        ) onlyOwner(_senderId, _productId) public {
+        ) public onlyOwner(_senderId, _productId) {
         
         // Update Trace array
         productTrace[_productId].push(Trace(_time, _location, _senderId, _receiverId));
         
+        // update products owned
+        removeFromOwner(_senderId, _productId);
+        productsOwned[_receiverId].push(_productId);
+        
         // Change owner
         products[_productId].currentOwnerId = _receiverId;
         
-        // update products owned
-        bool flag = false;
-        for(uint i = 0; i < productsOwned[_senderId].length - 1; i++){
-            if(productsOwned[_senderId][i] == _productId){
-                flag = true;
-            }
-            if(flag){
-                productsOwned[_senderId][i] = productsOwned[_senderId][i+1];
-            }
-        }
-        productsOwned[_senderId].pop();
-        productsOwned[_receiverId].push(_productId);
         
-        
+    }
+    
+    function addToOwner(uint _parentId, uint _ownerId) public {
+        require(products[_parentId].currentOwnerId == _ownerId, "The old product was not yours");
+        productsOwned[_ownerId].push(_parentId);
     }
     
     function deleteProducts(uint[] memory _childrenIds, uint _ownerId) public {
         
         for(uint i=0; i < _childrenIds.length; i++){
             // remove from productsOwned
-            
-            bool flag = false;
-            for(uint j = 0; j < productsOwned[_ownerId].length - 1; j++){
-                
-                if(productsOwned[_ownerId][j] == _childrenIds[i]){
-                    flag = true;
-                }
-                
-                if(flag){
-                    productsOwned[_ownerId][j] = productsOwned[_ownerId][j+1];
-                }
-            }
-            productsOwned[_ownerId].pop();
-            
+            removeFromOwner(_ownerId, _childrenIds[i]);
             
             //remove from parents    
             
             for( uint j = 0; j < products[_childrenIds[i]].parentId.length; j++){  // maybe change later
                 
-                flag = false;
+                bool flag = false;
                 Product memory p = products[uint(products[_childrenIds[i]].parentId[j])];
                 for(uint k = 0; k < p.childrenId.length - 1; k++){
                     
@@ -164,33 +148,44 @@ contract SupplyChain {
                         p.childrenId[k] = p.childrenId[k+1];
                     }
                 }
-
+                
                 products[uint(products[_childrenIds[i]].parentId[j])] = p;
                 products[uint(products[_childrenIds[i]].parentId[j])].childrenId.pop();
             }
             
             //remove from products
+            
             delete products[_childrenIds[i]];
         }
     }
     
     // split
     
-    function split(uint _productId, string[] memory _encProdProps, uint _ownerId) public {
-        Product memory p = products[_productId];
-        for(uint i = 0; i < _encProdProps.length; i++){
-            // make a new product
-            // uint[] memory t1;
-            // = [uint(_productId)];
-            // t1.push(uint(_productId));
-            // uint newId = addProduct(p.productName, t1,  [], _ownerId, _encProdProps[i]);
+    
+    
+    // function split(int[] memory _productId, string[] memory _encProdProps, uint _ownerId) public returns(uint[] memory finalProducts) {
+    //     string memory p = products[uint(_productId[0])].productName;
+    //     uint[] memory t1;
+    //     // int[] memory t2;
+    //     // t2[t2.length] = _productId;
+    //     for(uint i = 0; i < _encProdProps.length; i++){
+    //         uint newId = addProduct(p, _productId, t1, _ownerId, _encProdProps[i]);
             
-        }
-        // remove from owned
-        removeFromOwner(_ownerId, _productId);
+    //         finalProducts;
+    //     }
+    //     // remove from owned
+    //     removeFromOwner(_ownerId, uint(_productId[0]));
         
-    }
+    // }
     
     
     // function for tracing
+    
+    // function makeTrace(int _productId) public pure returns(Product[] memory p){
+    //     p = products;
+    //     // flag = true;
+    //     // while (flag) {
+            
+    //     // }
+    // }
 }
---
+++ w3.py
@@ -0,0 +1,124 @@
+from web3 import Web3
+from flask import Flask
+import json
+
+my_provider = Web3.HTTPProvider('http://127.0.0.1:7545')
+
+w3 = Web3(my_provider);
+
+w3.eth.defaultAccount = w3.eth.accounts[0];
+print(w3.isConnected())
+
+my_abi = [
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "initCount",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [],
+        "name": "func",
+        "outputs": [
+            {
+                "components": [
+                    {
+                        "internalType": "uint256[]",
+                        "name": "a1",
+                        "type": "uint256[]"
+                    },
+                    {
+                        "internalType": "uint256[]",
+                        "name": "a2",
+                        "type": "uint256[]"
+                    }
+                ],
+                "internalType": "struct Coursetro.IDK",
+                "name": "o",
+                "type": "tuple"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getCount",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "getInstructor",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "incrementCount",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "string",
+                "name": "_fName",
+                "type": "string"
+            },
+            {
+                "internalType": "uint256",
+                "name": "_age",
+                "type": "uint256"
+            }
+        ],
+        "name": "setInstructor",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+]
+
+greeter = w3.eth.contract(address="0x8e4b7b314A8Fe447A06f0D2aC8444EC868b16961", abi=my_abi)
+
+app = Flask(__name__)
+
+@app.route('/view')
+def home():
+
+    tx_hash = greeter.functions.incrementCount().transact()
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+    
+    print(tx_receipt)
+    
+    # calling
+    x = greeter.functions.func().call()
+    print(x)
+    d = {'value':json.dumps(x)}
+    print(d)
+    return d
