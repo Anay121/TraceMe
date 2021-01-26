6b426f91041534dd3c40b023fe6e29874a1c8fd0
anay121 <anay.kulkarni10@gmail.com>
2021-01-26 12:02:09 +0530

    Merge cofs

--- trace_me/lib/pages/mainHomePage.dart
+++ trace_me/lib/pages/mainHomePage.dart
@@ -53,6 +53,10 @@ class MainHomePage extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height / 40),
+                RaisedButton(
+                  onPressed: () =>
+                      {Navigator.pushNamed(context, "TransferPage")},
+                )
               ],
             ),
           ),
--- trace_me/lib/routes/router.dart
+++ trace_me/lib/routes/router.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:trace_me/pages/login.dart';
 import 'package:trace_me/pages/mainHomePage.dart';
 import 'package:trace_me/pages/registerPage.dart';
+import 'package:trace_me/pages/transferPage.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   print(settings.arguments);
   print(settings.name);
-  // final arguments = settings.arguments;
+  final arguments = settings.arguments;
   switch (settings.name) {
     case "/":
       return MaterialPageRoute(builder: (context) => MainHomePage());
@@ -14,6 +15,8 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (context) => LoginPage());
     case 'RegisterPage':
       return MaterialPageRoute(builder: (context) => RegisterPage());
+    case 'TransferPage':
+      return MaterialPageRoute(builder: (context) => TransferPage(arguments));
     default:
       return MaterialPageRoute(builder: (context) => LoginPage());
   }
